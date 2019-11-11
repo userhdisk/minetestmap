3318cd6b66c1d8d2662809cc0cd6a627b5958527
userhdisk <none>
2019-11-11 20:57:00 +0000

    bugfix ocr script

--- ocr.sh
+++ ocr.sh
@@ -2,5 +2,5 @@
 OUT_DIR=$(mktemp -d)
 convert -crop 666x313+808+137 $1 $OUT_DIR/out.png ; convert -bordercolor White -border 10x10;convert $OUT_DIR/out.png -channel RGB -negate $OUT_DIR/out.png
 tesseract --dpi 300 -c tessedit_char_whitelist='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ)(-,' $OUT_DIR/out.png $OUT_DIR/text_$1
-cat $OUT_DIR/text_$1
+cat $OUT_DIR/text_$1.txt
 
